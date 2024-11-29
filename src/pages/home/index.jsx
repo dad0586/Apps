e8304423acbe6d3031d90570e52b0072750fa9f5
otaboy <user@MacBook-Pro-user.local>
2024-11-30 01:59:11 +0500

    json

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,5 +1,20 @@
+import { useEffect, useState } from "react"
 import "./main.scss"
+import axios from "axios"
 const Home = () => {
+  const [data, setData] = useState([])
+  console.log(data);
+
+
+  useEffect(() => {
+    axios.get('http://localhost:5010/products')
+      .then((res) => {
+        setData(res?.data)
+
+      })
+  }, [])
+
+
   return (
     <>
       <div>
@@ -12,6 +27,15 @@ const Home = () => {
               <th>4</th>
               <th>5</th>
             </tr>
+            {data?.map((item, i) => (
+              <tr key={i}>
+                <th>1</th>
+                <th>2</th>
+                <th>3</th>
+                <th>4</th>
+                <th>5</th>
+              </tr>
+            ))}
           </table>
         </div>
       </div>
