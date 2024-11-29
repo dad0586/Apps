63578017325a68989e6f05066eba8b02b4ffe434
otaboy <user@MacBook-Pro-user.local>
2024-11-29 23:34:05 +0500

    full

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import './assets/styles/main.scss'
+import Home from './pages/home';
 
 function App() {
   return (
     <>
       <Router>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Home/>} />
         </Routes>
       </Router>
     </>
