15cfe255d9539f81fd3402021a3d0ddfd1d04afb
otaboy <user@MacBook-Pro-user.local>
2024-11-29 23:52:25 +0500

    Link

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import './assets/styles/main.scss'
 import Home from './pages/home';
+import Header from './components/header';
 
 function App() {
   return (
     <>
       <Router>
+        <Header/>
         <Routes>
           <Route path="/" element={<Home/>} />
         </Routes>
