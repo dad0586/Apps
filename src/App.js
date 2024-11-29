54322f74b16f3e324be03e2e0e49d340e94b6ff0
otaboy <user@Mac.bbrouter>
2024-11-29 19:42:55 +0500

    savol1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-import Header from './components/header/header';
-import Footer from './components/footer/footer';
-import Home from './pages/home/home';
-import About from './pages/about/about';
 import './assets/styles/main.scss'
-import Navbar from './components/navbar/navbar';
-import Sidebar from './components/sidebar';
-import MyForm from './pages/form';
-import MovieInfo from './pages/movie-info';
 
 function App() {
   return (
     <>
       <Router>
-        <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/form" element={<MyForm />} />
-          <Route path="/movie/:movieId" element={<MovieInfo />} />
         </Routes>
       </Router>
     </>
